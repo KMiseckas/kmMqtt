f9bef39ccdf5b3ef5b1b52149968f206ba7c0cd1
Klaudijus <klaudmise@gmail.com>
2025-10-28 19:21:52 +0000

    Define fixes.

--- cmake/options.cmake
+++ cmake/options.cmake
@@ -9,6 +9,8 @@ option(BUILD_EXAMPLES "Should examples be built?" ON)
 option(ENABLE_WARNINGS_AS_ERRORS "Should enable max warnings level as errors?" ON)
 option(ENABLE_LOGS "Should logging be enabled?" ON)
 
+option(ENABLE_BYTEBUFFER_SBO "Enable the SBO for ByteBuffer class. Buffer SBO size set to 256 bytes as default." ON)
+
 #MQTT Encoding
 option(FORCE_ADD_PROPERTIES "Should properties for packets be added as part of encoding regardless 
 whether they are empty to force properties to always have a value when they reach the mqtt broker. 
@@ -24,7 +26,9 @@ message(STATUS "  BUILD_EXAMPLES: ${BUILD_EXAMPLES}")
 message(STATUS "  ENABLE_WARNINGS_AS_ERRORS: ${ENABLE_WARNINGS_AS_ERRORS}")
 message(STATUS "  FORCE_ADD_PROPERTIES: ${FORCE_ADD_PROPERTIES}")
 message(STATUS "  ENABLE_LOGS: ${ENABLE_LOGS}")
+message(STATUS "  ENABLE_BYTEBUFFER_SBO: ${ENABLE_BYTEBUFFER_SBO}")
 
 
 # Sets
 set(LOG_BUFFER_SIZE 2048 CACHE STRING "Fixed log buffer size in bytes")
+set(BYTEBUFFER_SBO_MAX_SIZE 256 CACHE STRING "Fixed buffer on stack max size for ByteBuffer class (Used by packets on receive and send)")
